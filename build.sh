03f8e6b982878ccce1cb81f638ab754af388bb5f
Yanan Tang <ytang.programmer@gmail.com>
2022-05-22 08:47:20 +0800

    add entt  ECS third party lib

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-COMPILE="g++ -std=c++17 ./src/*.cpp ./lib/glad/src/glad.c -I./include -I ./lib/glad/include -o ./bin/prog -lsdl2 -lsdl2_image -ldl"
+COMPILE="g++ -std=c++17 -g ./src/*.cpp ./lib/glad/src/glad.c -I./include -I./lib/glad/include -I./lib/entt/include -o ./bin/prog -lsdl2 -lsdl2_image -ldl"
 echo ${COMPILE}
 eval $COMPILE
