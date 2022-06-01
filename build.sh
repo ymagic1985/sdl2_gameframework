d047082a221055ce8c2cd034172562447820c678
Yanan Tang <ytang.programmer@gmail.com>
2022-06-01 11:12:21 +0800

    ignore bin folder

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 #PRECOMPILEHEADER="g++ -std=c++17 -x c++-header -o ./include/mzpch.h.gch -c ./include/mzpch.h -I./lib/spdlog/include"
-COMPILE="clang++ -std=c++17 -v -g ./engine/src/*.cpp ./sandbox/src/*.cpp  ./engine/lib/glad/src/glad.c -I./sandbox/include -I./engine/include -I./engine/lib/spdlog/include -I./engine/lib/stb -I./engine/lib/glm -I./engine/lib/glad/include -I./engine/lib/entt/include -o ./bin/prog -lsdl2 -lsdl2_image -ldl"
+COMPILE="clang++ -std=c++17 -g ./engine/src/*.cpp ./sandbox/src/*.cpp  ./engine/lib/glad/src/glad.c -I./sandbox/include -I./engine/include -I./engine/lib/spdlog/include -I./engine/lib/stb -I./engine/lib/glm -I./engine/lib/glad/include -I./engine/lib/entt/include -o ./bin/prog -lsdl2 -lsdl2_image -ldl"
 
 #echo ${PRECOMPILEHEADER}
 #eval $PRECOMPILEHEADER
